488b88c5b979cb260ffb663a90f9efc0ad2dafc0
Eyjolfur Vatne <eyjolfur@arko.no>
2017-07-25 11:50:37 +0200

    fix(mdc-drawer-header): correctly detect parent drawer type

--- src/bridge/drawer/header.ts
+++ src/bridge/drawer/header.ts
@@ -1,17 +1,16 @@
-import { inject, customElement } from 'aurelia-framework';
+import { customElement } from 'aurelia-framework';
 import { getLogger, Logger } from 'aurelia-logging';
 import * as drawerCommon from './common';
 import * as util from '../util';
 
 @customElement('mdc-drawer-header')
-@inject(Element)
 export class MdcDrawerHeader {
   private log: Logger;
 
   private elementHeader: HTMLElement;
   private elementContent: HTMLElement;
 
-  constructor(private element: Element) {
+  constructor() {
     this.log = getLogger('mdc-drawer-header');
   }
 
@@ -19,15 +18,15 @@ export class MdcDrawerHeader {
   private unbind() { /** */ }
 
   private attached() {
-    if (drawerCommon.isPermanentDrawer(this.element)) {
+    if (drawerCommon.isPermanentDrawer(this.elementHeader)) {
       this.elementHeader.classList.add('mdc-permanent-drawer__header');
       this.elementContent.classList.add('mdc-permanent-drawer__header-content');
     }
-    if (drawerCommon.isPersistentDrawer(this.element)) {
+    if (drawerCommon.isPersistentDrawer(this.elementHeader)) {
       this.elementHeader.classList.add('mdc-persistent-drawer__header');
       this.elementContent.classList.add('mdc-persistent-drawer__header-content');
     }
-    if (drawerCommon.isTemporaryDrawer(this.element)) {
+    if (drawerCommon.isTemporaryDrawer(this.elementHeader)) {
       this.elementHeader.classList.add('mdc-temporary-drawer__header');
       this.elementContent.classList.add('mdc-temporary-drawer__header-content');
     }
