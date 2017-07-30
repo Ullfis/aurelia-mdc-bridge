48bb704eea983a6ee731566f5a5e8bebe459af76
Eyjolfur Vatne <eyjolfur@arko.no>
2017-07-30 10:39:14 +0200

    fix(mdc-dialog): add on-opened and on-closed events

--- src/bridge/dialog/dialog.ts
+++ src/bridge/dialog/dialog.ts
@@ -1,6 +1,6 @@
 import { inject, bindable, bindingMode, customElement } from 'aurelia-framework';
 import { getLogger, Logger } from 'aurelia-logging';
-import { MDCDialog } from '@material/dialog';
+import { MDCDialog, MDCDialogFoundation, util as MDCUtil } from '@material/dialog';
 import * as util from '../util';
 
 export interface IMdcDialogClickEvent extends CustomEvent {
@@ -22,9 +22,11 @@ export class MdcDialog {
   @bindable() public accept = '';
   @bindable() public cancel = '';
   @bindable() public scrollable = false;
+  @bindable() public focusAt: HTMLElement;
   private log: Logger;
   private diagElement: HTMLElement;
-  private mdcElement;
+  private mdcElement: MDCDialog;
+  private mdcDialogFoundation: MDCDialogFoundation;
   private controlId = '';
 
   constructor(private element: Element) {
@@ -46,12 +48,24 @@ export class MdcDialog {
   private attached() {
     this.scrollableChanged(this.scrollable);
     this.mdcElement = new MDCDialog(this.diagElement);
+    this.mdcDialogFoundation = this.mdcElement.foundation_.adapter_;
+    this.mdcDialogFoundation.registerTransitionEndHandler(this.onTransitionEnd.bind(this));
     this.mdcElement.listen('MDCDialog:accept', this.onButtonAccept.bind(this));
     this.mdcElement.listen('MDCDialog:cancel', this.onButtonCancel.bind(this));
+
+    // not working with mdc-textfield (it works with input type="text")
+    // <input type="text" ref="focusMeOnOpen" ..
+    // <mdc-dialog focus-at.bind="focusMeOnOpen" ..
+    if (this.focusAt) {
+      this.log.debug('this.focusAt:', this.focusAt);
+      this.mdcElement.focusTrap_ = MDCUtil.createFocusTrapInstance(this.mdcElement.dialogSurface_, this.focusAt);
+    }
   }
   private detached() {
     this.mdcElement.unlisten('MDCDialog:accept', this.onButtonAccept.bind(this));
     this.mdcElement.unlisten('MDCDialog:cancel', this.onButtonCancel.bind(this));
+    this.mdcDialogFoundation.deregisterTransitionEndHandler(this.onTransitionEnd.bind(this));
+    this.mdcDialogFoundation = null;
     this.mdcElement.destroy();
   }
 
@@ -61,8 +75,18 @@ export class MdcDialog {
   private onButtonCancel() {
     util.fireEvent(this.diagElement, 'on-click', false);
   }
-
   private scrollableChanged(newValue) {
     this.scrollable = util.getBoolean(newValue);
+  }
+  private onTransitionEnd(evt) {
+    if (this.mdcDialogFoundation.isDialog(evt.target)) {
+      if (evt.propertyName === 'opacity') {
+        if (this.mdcElement.open) {
+          util.fireEvent(this.diagElement, 'on-opened', {});
+        } else {
+          util.fireEvent(this.diagElement, 'on-closed', {});
+        }
+      }
+    }
   }
 }
