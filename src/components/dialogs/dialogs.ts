7ce2aa445d7d3a2e81257dc3008d5e90bc59d968
Eyjolfur Vatne <eyjolfur@arko.no>
2017-07-30 10:41:28 +0200

    doc(dialogs): describe opened/closed events

--- src/components/dialogs/dialogs.ts
+++ src/components/dialogs/dialogs.ts
@@ -1,10 +1,14 @@
-import { MdcDialog, IMdcDialogClickEvent } from '../../bridge/index';
+import { MdcDialog, IMdcDialogClickEvent, MdcTextfield } from '../../bridge/index';
 export class Dialogs {
   private eventCounter = 0;
   private dialog: MdcDialog;
   private dialog2: MdcDialog;
+  private dialog3: MdcDialog;
   private acceptClicked1: boolean;
   private acceptClicked2: boolean;
+  private openCloseStatus3 = '';
+  private focusMeElement: MdcTextfield;
+
   private onDialogClick1(event: IMdcDialogClickEvent) {
     this.acceptClicked1 = event.detail;
     this.eventCounter++;
@@ -18,5 +22,15 @@ export class Dialogs {
   }
   private showDialog2() {
     this.dialog2.show(true);
+  }
+  private showDialog3() {
+    this.dialog3.show(true);
+  }
+  private onOpened3() {
+    this.openCloseStatus3 = 'opened event fired';
+    this.focusMeElement.focus();
+  }
+  private onClosed3() {
+    this.openCloseStatus3 = 'closed event fired';
   }
 }
